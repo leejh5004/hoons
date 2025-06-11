175b4ca6771c318732ad325c0fcd86c7cbcbd4a8
leejh5004 <58938121+leejh5004@users.noreply.github.com>
2025-06-11 13:46:37 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // 관리자 계정 (이메일/비밀번호)
 const ADMIN_EMAIL = 'admin@admin.com';
 const ADMIN_PASSWORD = 'admin1234';
@@ -259,21 +258,4 @@ window.onload = function() {
   userName.textContent = '';
   addBtnBox.style.display = 'none';
   searchBox.style.display = 'none';
-};
-
-// Firebase 설정
-const firebaseConfig = {
-  apiKey: "AIzaSyDu_DVHYKm8kBLNT04V1NdXyqP2K-ZU8qC1E",
-  authDomain: "hoons-a02bc.firebaseapp.com",
-  projectId: "hoons-a02bc",
-  storageBucket: "hoons-a02bc.appspot.com",
-  messagingSenderId: "129637551362",
-  appId: "1:129637551362:web:3bb6f715fdb3a2cd9661b"
-};
-
-// Firebase 초기화
-firebase.initializeApp(firebaseConfig);
-
-// Firebase 서비스 초기화
-const auth = firebase.auth();
-const db = firebase.firestore(); 
+}; 
