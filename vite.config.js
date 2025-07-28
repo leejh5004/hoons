6d81ae8b68ede230d95a40f2c1878fe2e55148b9
leejh5004 <58938121+leejh5004@users.noreply.github.com>
2025-07-28 23:38:17 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? '/hoons/' : '/',
-    plugins: [react()],
+export default defineConfig({
+  build: {
+    outDir: 'dist'
   }
 }) 
